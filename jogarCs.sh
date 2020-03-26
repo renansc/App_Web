fbc52ad4e5ab467c2d791403ab1a6e5371b0ada6
renansc <renanrocks2009@gmail.com>
2020-03-26 18:31:58 -0300

    arquvio de jogar e abrir server no linux

---
+++ jogarCs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#executa wine e direciona para executavel do jogo
+echo 'usando wine para executar na pasta ~/.wine/drive_c/Program Files (x86)/Counter-Strike 1.6'
+echo  `wine ~/.wine/drive_c/'Program Files (x86)'/'Counter-Strike 1.6'/cstrike.exe`
+
---
+++ serverCs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#entra na pasta do cs executa wine e direciona para executavel do jogo
+echo 'Executando na pasta do cstrike com wine ~/.wine/drive_c/Program Files (x86)/Counter-Strike 1.6'
+echo `cd ~/.wine/drive_c/'Program Files (x86)'/'Counter-Strike 1.6'  && wine hlds.exe`
+
